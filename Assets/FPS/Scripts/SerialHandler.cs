8dd87cb38a7c25d750b021a98a964d54b7afc51a
Yusuke Yamazaki <yus3594@gmail.com>
2024-01-21 11:11:55 +0900

    init

---
+++ Assets/FPS/Scripts/SerialHandler.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.IO.Ports;
+using System.Threading;
+using UnityEngine;
+
+// https://qiita.com/ZAP_xgame/items/9487aeddd3b9fa9a8f1d
+
+public class SerialHandler : MonoBehaviour
+{
+    public delegate void SerialDataReceivedEventHandler(string message);
+    public event SerialDataReceivedEventHandler OnDataReceived;
+
+    public string portName = "COM3";
+    public int baudRate = 115200;
+
+    private SerialPort serialPort_;
+    private Thread thread_;
+    private bool isRunning_ = false;
+
+    private string message_;
+    private bool isNewMessageReceived_ = false;
+
+    void Awake()
+    {
+        Open();
+        Write("hoge");
+    }
+
+    void Update()
+    {
+        if (isNewMessageReceived_)
+        {
+            OnDataReceived(message_);
+        }
+        isNewMessageReceived_ = false;
+    }
+
+    void OnDestroy()
+    {
+        Close();
+    }
+
+    private void Open()
+    {
+        serialPort_ = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
+        //serialPort_ = new SerialPort(portName, baudRate);
+        serialPort_.Open();
+
+        isRunning_ = true;
+
+        thread_ = new Thread(Read);
+        thread_.Start();
+    }
+
+    private void Close()
+    {
+        isNewMessageReceived_ = false;
+        isRunning_ = false;
+
+        if (thread_ != null && thread_.IsAlive)
+        {
+            thread_.Join();
+        }
+
+        if (serialPort_ != null && serialPort_.IsOpen)
+        {
+            serialPort_.Close();
+            serialPort_.Dispose();
+        }
+    }
+
+    private void Read()
+    {
+        while (isRunning_ && serialPort_ != null && serialPort_.IsOpen)
+        {
+            try
+            {
+                message_ = serialPort_.ReadLine();
+                isNewMessageReceived_ = true;
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+
+    public void Write(string message)
+    {
+        try
+        {
+            serialPort_.Write(message);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+}
